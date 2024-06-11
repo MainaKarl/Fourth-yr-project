3c2f5c254c39f574d9eb9417afcfb2ab8c91f3f9
MainaKarl <mainakarlg@gmail.com>
2024-06-11 21:57:49 +0300

    Api deployment

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def transcribe_video():
     return jsonify({'transcription': transcription})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
